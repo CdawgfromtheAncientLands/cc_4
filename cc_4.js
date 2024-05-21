9382bebe0a140c9dc55e9a33e3a137d1f824a6c5
CdawgfromtheAncientLands <cmrbusiness123456@gmail.com>
2024-05-21 11:24:41 -0400

    Override accelerate method in EV class to adjust speed and charge

--- cc_4.js
+++ cc_4.js
@@ -3,6 +3,9 @@
 accelerate_KPHvalue = 10
 brake_KPHvalue = 5
 
+EVaccelerate_KPHvalue = 20
+EVaccelerate_chargePercentDecrease = 1
+
 class Car {
     constructor(make, currentKPH) {
       this.make = make;
@@ -22,10 +25,18 @@ class Car {
 //Extend the Car class to create an EV class that also includes a charge property representing the current battery charge in percentage.
 class EV extends Car {
     constructor(charge) {
-        this.charge = charge
+        this.charge = charge;
     }
     //Implement a chargeBattery method in the EV class that takes an argument chargeTo and sets the battery charge to this value.
     chargeBattery(chargeTo) {
-        this.charge = chargeTo
+        this.charge = chargeTo;
     }
+    //Override the accelerate method in the EV class to increase the car's speed by 20 km/h and decrease the battery charge by 1%.
+    accelerate() {
+        this.currentKPH += accelerate_KPHvalue;
+        this.charge -= EVaccelerate_chargePercentDecrease;
+        //Log the new speed and charge level in the console with a message like: "Tesla going at 140 km/h, with a charge of 22%"
+        console.log (this.make + "going at "+ this.currentKPH + "km/h, with a charge of " + this.charge + "%.");
+    }
+    
 }
