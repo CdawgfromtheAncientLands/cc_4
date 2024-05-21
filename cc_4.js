70c306ecc9cf12867f256bd5ff98cabd5f3d6f60
CdawgfromtheAncientLands <cmrbusiness123456@gmail.com>
2024-05-21 11:15:43 -0400

    Create EV class extending Car with additional charge property.

---
+++ cc_4.js
@@ -0,0 +1,27 @@
+//U9414-0233
+
+accelerate_KPHvalue = 10
+brake_KPHvalue = 5
+
+class Car {
+    constructor(make, currentKPH) {
+      this.make = make;
+      this.currentKPH = Number(currentKPH);
+    }
+    //Acceleration method
+    accelerate() {
+        this.currentKPH += accelerate_KPHvalue;
+        console.log ("The new speed of the " + this.make + " after accelerating " + accelerate_KPHvalue + "KM/h is " + this.currentKPH + "KM/h.");
+    }
+    //Brake method
+    brake() {
+        this.currentKPH -= brake_KPHvalue;
+        console.log ("The new speed of the " + this.make + " after braking " + brake_KPHvalue + "KM/h is " + this.currentKPH + "KM/h.");
+    }
+}
+//Extend the Car class to create an EV class that also includes a charge property representing the current battery charge in percentage.
+class EV extends Car {
+    constructor(charge) {
+        this.charge = charge
+    }
+}
